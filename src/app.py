addc6c157ed0a1b0c1593ea5c1ca6aa0594a3ff4
Salath Joseph <127238115+salathjoseph@users.noreply.github.com>
2024-05-18 01:15:39 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from models import db
 from controllers.order_controller import order_bp
+from controllers.tracking_controller import tracking_bp
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///orders.db'
@@ -8,9 +9,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-app.register_blueprint(order_bp, url_prefix='/orders')
+app.register_blueprint(order_bp, url_prefix='/orders') ## post call
+app.register_blueprint(tracking_bp, url_prefix='/tracking') ## Get call
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
